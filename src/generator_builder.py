1bbde636c5b28a8778ee69d796887de09f9291b5
andrey <andrew@facepunk.com>
2024-02-24 22:44:30 +0400

    Update generator builder to return chat engine instead of a query engine

--- src/generator_builder.py
+++ src/generator_builder.py
@@ -2,7 +2,6 @@ from src.core.retriever import build_retriever
 from src.core.postprocessor import build_context_filter
 from src.core.query_engine import build_query_engine
 
-from pydantic.v1 import BaseModel
 from llama_index.core.indices.base import BaseIndex
 from llama_index.core.base.base_query_engine import BaseQueryEngine
 from llama_index.core.schema import BaseNode
@@ -11,9 +10,14 @@ from llama_index.core.llms import LLM
 from llama_index.core.embeddings import BaseEmbedding
 from llama_index.llms.azure_openai import AzureOpenAI
 from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
+from llama_index.core.chat_engine import CondenseQuestionChatEngine
+from llama_index.core.chat_engine.types import BaseChatEngine
+
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_openai import AzureChatOpenAI
+
 from typing import Dict
+from pydantic.v1 import BaseModel
 
 
 class LLMConfig(BaseModel):
@@ -62,13 +66,14 @@ def build_generator(
     index: BaseIndex,
     full_nodes_dict: Dict[str, BaseNode],
     llm_config: LLMConfig,
-) -> BaseQueryEngine:
+    verbose: bool = False,
+) -> BaseChatEngine:
     # 1. Retriever that performs search over index
     retriever = build_retriever(
         index=index,
         full_nodes_dict=full_nodes_dict,
         llm=llm_config.llamaindex_light,
-        verbose=False,
+        verbose=verbose,
         top_k=10,
     )
 
@@ -87,4 +92,11 @@ def build_generator(
         langchain_heavy=llm_config.langchain_heavy,
     )
 
-    return query_engine
+    # 4. Chat engine that condences chat history into a single search query
+    chat_engine = CondenseQuestionChatEngine.from_defaults(
+        query_engine=query_engine,
+        llm=llm_config.llamaindex_light,
+        verbose=verbose,
+    )
+
+    return chat_engine
